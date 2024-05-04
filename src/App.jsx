7bb934994a55c22c26fb582c6dca9827c1b49ef0
Shiwang Sanger <shiwang21sanger@gmail.com>
2024-05-04 14:30:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react'
+import Card from "./components/Card"
+
+function App() {
+  const options = ["Politics", "Wwe", "Sports", "Bollywood"]
+  const [articles, setArticles] = useState([])
+  const [input, setInput] = useState("")
+  const apiKey = '8585f6668f1944deb2545c040f39002e';
+  let searchItem = async (search) => {
+    const url = `https://newsapi.org/v2/everything?q=${search}&apiKey=${apiKey}`
+    try {
+      let res = await fetch(url);
+      let data = await res.json();
+      let news = data.articles
+      setArticles(news)
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  useEffect(() => {
+    searchItem("India")
+  }, [])
+
+
+  return (
+    <>
+      <div className='h-screen w-full'>
+        {/* NAVBAR */}
+        <div>
+          <nav className='w-full h-16 bg-gray-500 flex justify-center items-center'>
+            <div className='w-2/6 h-full text-center py-2'>
+              <span className='text-5xl cursor-pointer font-bold font-serif text-white' onClick={() => searchItem("India")}>AKH</span>
+              <span className='text-5xl cursor-pointer font-bold font-serif text-black' onClick={() => searchItem("India")}>BAR</span>
+            </div>
+            <ul className='text-black flex justify-evenly w-4/6 items-center'>
+              {options.map((opt) => {
+                return (<li key={opt} className='text-white cursor-pointer font-bold font-mono text-2xl hover:text-black transition-all duration-300'
+                  onClick={() => searchItem(opt)}>{opt}</li>)
+              })
+              }
+            </ul>
+            <div className='flex justify-center items-center w-1/3 h-full'>
+              <input type="text" className='w-5/6 h-1/2 border-2 border-black rounded-r-none border-r-0  rounded-lg px-4 text-black font-semibold
+              ' placeholder='Search'
+                value={input}
+                onChange={(e) => setInput(e.target.value)} />
+              <button className='w-1/12 bg-red-500 h-1/2 border-2 border-black border-l-0 rounded-r-lg  text-center' onClick={() => searchItem(input)}><i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+          </nav>
+        </div>
+
+        {/* ARTICLE CARDS */}
+        <div className='w-full h-full flex flex-wrap justify-evenly items-center my-12 gap-y-12'>
+          {articles.map((currElem, idx) => {
+            return (
+              <Card key={idx} display={currElem} />
+            )
+          })
+
+          }
+        </div>
+      </div >
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Card({ display }) {
+    const { urlToImage, description, title, url
+    } = display
+
+    if(!display.urlToImage) return
+    else return (
+        <>
+            <div>
+                <div class="card  max-w-sm bg-gray-400 border border-gray-800 rounded-lg hover:-translate-y-2 transition-all duration-300 shadow-black shadow-md" style={{height:"70vh"}}>
+                    <a href={url} target='_blank'>
+                        <img class="rounded-t-lg h-64 w-full text-white " src={`${urlToImage}`} alt="image not found" />
+                    </a>
+                    <div class="p-5 ">
+                        <a href={url} target='_blank'>
+                            <h5 class="mb-2 h-24 overflow-hidden text-2xl font-bold tracking-tight text-black">{title}</h5>
+                        </a>
+                        <p class="mb-3 font-normal h-12 overflow-hidden text-black">{description}</p>
+                        <a href={`${url}`} target='_blank' class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                            Read more➡️
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Card
